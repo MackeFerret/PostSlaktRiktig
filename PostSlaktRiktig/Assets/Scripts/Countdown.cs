a4f057286bb983e593dbeeef6d17af6849e23789
MackeFerret <maxhulting@hotmail.com>
2022-08-03 10:38:25 +0200

    Timer

--- PostSlaktRiktig/Assets/Scripts/Countdown.cs
+++ PostSlaktRiktig/Assets/Scripts/Countdown.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Countdown : MonoBehaviour
 {
+    public Text TimerText;
+    public static float Timer = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class Countdown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Timer -= Time.deltaTime;
+        if(Timer < 0) Timer = 0;
+        TimerText.text = "Time left: " + Mathf.Round(Timer);
+        isDead();
+    }
+    public void isDead()
+    {
+        if (Timer == 0) Time.timeScale -= Time.deltaTime;
+        if (Time.timeScale == 0) Time.timeScale = 0;
     }
 }
