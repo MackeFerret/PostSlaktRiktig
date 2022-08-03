29bcdfb2a83bcad57b4ed00cce1d81a1c96d5247
MackeFerret <maxhulting@hotmail.com>
2022-08-03 14:54:12 +0200

    Game Over

--- PostSlaktRiktig/Assets/Scripts/Countdown.cs
+++ PostSlaktRiktig/Assets/Scripts/Countdown.cs
@@ -6,18 +6,22 @@ using UnityEngine.UI;
 public class Countdown : MonoBehaviour
 {
     public Text TimerText;
-    public static float Timer = 20;
+    public  float Timer = 60;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Timer -= Time.deltaTime;
-        if(Timer < 0) Timer = 0;
+        if (Timer < 0)
+        {
+            Timer = 0;
+            isDead();
+        }
         TimerText.text = "Time left: " + Mathf.Round(Timer);
         isDead();
     }
---
+++ PostSlaktRiktig/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public GameObject EndScreen1;
+
+    public Countdown timer;
+   
+    void Start()
+    {
+        EndScreen1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer.Timer <= 0)
+        {
+            EndScreen1.SetActive(true);
+        }
+       
+    }
+}
