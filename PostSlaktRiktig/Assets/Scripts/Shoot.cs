ea2dce74b7a78dded9f04423bcdc7f70eb0f60db
MackeFerret <maxhulting@hotmail.com>
2022-08-04 13:04:27 +0200

    Shooting

--- PostSlaktRiktig/Assets/Scripts/Shoot.cs
+++ PostSlaktRiktig/Assets/Scripts/Shoot.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    public Transform firePoint;
+    public GameObject ammo;
+    public float bulletForce = 20f;
+    private float reload = 1;
+    public float timer = 1;
+    Rigidbody2D rb;
+    Vector2 mousePos;
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,23 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //When reload = 1 you are able to spawn ammo
+        if (Input.GetButton("Fire1") && reload == 1)
+        {
+            GameObject bullet = Instantiate(ammo, firePoint.position, firePoint.rotation);
+            Rigidbody2D rbullet = bullet.GetComponent<Rigidbody2D>();
+            //rbullet.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+            reload = 0;
+        }
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            timer = 0;
+            if (Input.GetButton("Fire1"))
+            {
+                reload = 1;
+                timer = 1;
+            }
+        }
     }
 }
